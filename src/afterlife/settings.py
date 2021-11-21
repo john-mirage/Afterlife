9204c145dcc834c31ae415a21ae022e11eee7f67
chaosdynamix <chaosdynamix@posteo.net>
2021-11-21 15:33:49 +0100

    ref(django): del find_dotenv param in settings
    Delete the first parameter of the find_dotenv function.
    The parameter was the same as the default given by the function.

--- src/afterlife/settings.py
+++ src/afterlife/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from dotenv import dotenv_values, find_dotenv
 
-environment = dotenv_values(find_dotenv('.env'))
+environment = dotenv_values(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
