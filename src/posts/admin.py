9cb8d3a08a2446e02d53f83d3d8abd3d0b1b1e2d
chaosdynamix <chaosdynamix@posteo.net>
2021-11-22 19:01:50 +0100

    feat(app): add post tag and reference models in admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from posts.models import Post, Tag, Reference
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "published", "created_on", "last_updated")
+    list_editable = ("published",)
+    prepopulated_fields = {"slug": ("title",)}
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+class ReferenceAdmin(admin.ModelAdmin):
+    list_display = ("name", "url")
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Reference, ReferenceAdmin)
