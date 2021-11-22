0640045c3d860f069cf79b1f24de8215a3c4d6a8
chaosdynamix <chaosdynamix@posteo.net>
2021-11-22 18:57:26 +0100

    feat(app): add reference model in posts app

--- src/posts/models.py
+++ src/posts/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Reference(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Nom')
+    description = models.TextField(max_length=255, verbose_name='Description')
+    url = models.URLField(unique=True, verbose_name='Adresse')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Référence'
+    
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
