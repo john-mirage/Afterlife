2f318750df42a3458ba170f4ddb5fe697001df29
chaosdynamix <chaosdynamix@posteo.net>
2021-11-25 23:53:48 +0100

    feat(project): add index url

--- src/afterlife/urls.py
+++ src/afterlife/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('blog.urls')),
+    path('', views.afterlife_index, name='afterlifeIndex'),
     path('admin/', admin.site.urls),
 ]
