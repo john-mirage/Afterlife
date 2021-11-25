2cba3fb46abcf81d63d9808110cb5b23a0b0b093
chaosdynamix <chaosdynamix@posteo.net>
2021-11-25 23:56:36 +0100

    feat(app): add basic views in blog app

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'home.html')
+def blog_index(request):
+    return render(request, 'blog_index.html')
+
+def blog_post(request):
+    return HttpResponse('Blog post')
+
+def blog_story(request):
+    return HttpResponse('Blog story')
+
+def blog_tag(request):
+    return HttpResponse('Blog tag')
