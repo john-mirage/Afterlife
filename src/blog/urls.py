8d2a51f49c89b88c318daa30e98067f587f83d2d
chaosdynamix <chaosdynamix@posteo.net>
2021-11-25 23:56:00 +0100

    feat(app): add urls in blog app

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.blog_index, name='blogIndex'),
+    path('post/<slug:slug>/', views.blog_post, name='blogPost'),
+    path('story/<str:name>/', views.blog_story, name='blogStory'),
+    path('tag/<str:name>/', views.blog_tag, name='blogTag')
 ]
