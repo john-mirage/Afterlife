02f88dfc1614f3778e9dd814de75b9c28ddb2a90
chaosdynamix <chaosdynamix@posteo.net>
2021-11-25 23:57:34 +0100

    fix(project): edit location of static files and templates

--- src/afterlife/settings.py
+++ src/afterlife/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'afterlife.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'afterlife' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+    BASE_DIR / 'afterlife' / 'static',
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
