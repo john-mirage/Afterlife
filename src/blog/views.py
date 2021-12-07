fb0f75e86db88445aac9367dc187ee1060dceebc
chaosdynamix <chaosdynamix@posteo.net>
2021-12-07 17:04:06 +0100

    feat(project): add blog tags view

--- src/blog/views.py
+++ src/blog/views.py
@@ -10,5 +10,8 @@ def blog_post(request):
 def blog_story(request):
     return HttpResponse('Blog story')
 
+def blog_tags(request):
+    return render(request, 'pages/blog_tags.html')
+
 def blog_tag(request):
     return HttpResponse('Blog tag')
