6fa5b3e98a1b19b5d86b37336f36c91e773be44d
chaosdynamix <chaosdynamix@posteo.net>
2021-12-08 16:05:20 +0100

    fix(project): fix urls

--- src/afterlife/urls.py
+++ src/afterlife/urls.py
@@ -4,7 +4,8 @@ from afterlife import views
 
 urlpatterns = [
     path('', views.afterlife_index, name='afterlifeIndex'),
-    path('blog/', include('blog.urls')),
-    path('notebook/', include('notebook.urls')),
+    path('posts/', include('posts.urls')),
+    path('stories/', include('stories.urls')),
+    path('notes/', include('notes.urls')),
     path('admin/', admin.site.urls),
 ]
