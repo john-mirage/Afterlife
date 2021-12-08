57c0d53a5733a9f70008b4cb6eeffb36f25db540
chaosdynamix <chaosdynamix@posteo.net>
2021-12-08 16:03:30 +0100

    fix(app): delete notebook app

--- src/notebook/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from notebook import views
-
-urlpatterns = [
-    path('', views.notebook_index, name='notebookIndex'),
-]
--- src/notebook/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def notebook_index(request):
-    return render(request, 'pages/notebook_index.html')
