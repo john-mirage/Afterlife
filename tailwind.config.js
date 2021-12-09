faedda022aefcab44003e5098b3f911a3485e859
chaosdynamix <chaosdynamix@posteo.net>
2021-12-09 17:53:26 +0100

    feat(tailwind): restrict some screens to help dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
     fontFamily: {
       sans: [
         "'Inter'",
