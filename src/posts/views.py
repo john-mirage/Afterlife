a236cd14839eb3160ad45e94982416571cbff741
chaosdynamix <chaosdynamix@posteo.net>
2021-12-23 16:16:48 +0100

    ref(app): rename active tab context in posts views

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def posts_index(request):
-    context = {'active_navigation': 'posts'}
+    context = {'active_tab': 'posts'}
     return render(request, 'pages/posts_index.html', context)
 
 def posts_tags(request):
