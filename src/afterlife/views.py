6e46f4ab5eba8671b16bf54923feed5b2b192c78
chaosdynamix <chaosdynamix@posteo.net>
2021-12-30 17:41:05 +0100

    fix(project): fix template path for index view

--- src/afterlife/views.py
+++ src/afterlife/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def afterlife_index(request):
     context = {'active_tab': 'home'}
-    return render(request, 'pages/afterlife_index.html', context)
+    return render(request, 'pages/afterlife/index.html', context)
